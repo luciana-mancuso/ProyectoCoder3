ee5736fa6a1283d081419d0a9a903415900ff74d
luciana-mancuso <luciana.mancuso@live.com.ar>
2023-03-21 06:25:03 -0300

    tercera pre-entrega

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from AppCoder.views import productos, usuarios, clientes
+from AppCoder.views import producto, crear_producto, usuario, cliente
 
 urlpatterns = [
-    path('productos/', productos),
-    path('usuarios/', usuarios),
-    path('clientes/', clientes),
+    path('productos/', producto, name="AppCoderProductos"),
+    path('producto/<nombre>/<precio>', crear_producto),
+    path('usuarios/', usuario, name="AppCoderUsuarios"),
+    path('clientes/', cliente, name="AppCoderClientes"),
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,11 +3,24 @@ from django.http import HttpResponse
 from AppCoder.models import Producto, Usuario, Cliente
 
 # Create your views here.
-def productos(request):
-    return render(request, "index.html")
 
-def usuarios(request):
-    pass
+def producto(request):
+    all_productos=Producto.objects.all()
+    context={"productos": all_productos}
+    return render(request, "AppCoder/productos.html", context=context)
 
-def clientes(request):
-    pass
+def crear_producto(request, nombre, precio):
+    save_producto=Producto(nombre=nombre, precio=int(precio))
+    save_producto.save()
+    context={"nombre": nombre, "precio":precio}
+    return render(request, "AppCoder/save_producto.html", context)
+
+def usuario(request):
+    all_usuarios=Usuario.objects.all()
+    context={"usuarios": all_usuarios}
+    return render(request, "AppCoder/usuarios.html", context=context)
+
+def cliente(request):
+    all_clientes=Cliente.objects.all()
+    context={"clientes": all_clientes}
+    return render(request, "AppCoder/clientes.html", context=context)
